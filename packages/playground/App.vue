af7a821be571045d4912483b78b2afe5f2c1fe15
Nils Bergmann <nilsbergmann@noim.io>
2025-10-13 13:18:54 +0200

    dev: change playground

--- packages/playground/App.vue
+++ packages/playground/App.vue
@@ -1,14 +1,24 @@
 <!-- eslint-disable unused-imports/no-unused-imports -->
 <!-- Use this component to play with the main components -->
 <script setup lang="ts">
-import pdf14 from '@samples/issue126.pdf';
-import { VuePDF, usePDF } from '@tato30/vue-pdf';
+import testPdf from "@samples/AnimagiC_2024_Programmheft_V4.pdf";
+import { VuePDF, usePDF } from "@tato30/vue-pdf";
 
-const { pdf } = usePDF(pdf14)
+const { pdf } = usePDF(testPdf);
 </script>
 
 <template>
-  <div>
-    <VuePDF :pdf="pdf" annotation-layer />
+  <div style="background-color: black;">
+    <VuePDF
+      :scale="4"
+      :partial-viewbox="{
+        offsetX: 100,
+        offsetY: 0,
+        width: 500,
+        height: 500,
+      }"
+      :pdf="pdf"
+      text-layer
+    />
   </div>
 </template>
